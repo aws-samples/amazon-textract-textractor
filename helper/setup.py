224e6c5f6d65e79fce4de6b298b562f3ea35cebc
Simran Singh <ssnghkj@amazon.com>
2021-08-20 08:57:47 -0500

    add options to overlay WORD or LINE text and confidence overlays

--- helper/setup.py
+++ helper/setup.py
@@ -1,11 +1,51 @@
 import os
 import sys
 from setuptools import setup, find_packages
+from setuptools.command.install import install
 
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
+class FontInstaller(install):
+    def run(self):
+        self._copy_fonts()
+        install.run(self)
+
+    def _copy_fonts(self):
+        try:
+            import shutil
+
+            if sys.platform == "win32":
+                # check the windows font repository
+                # NOTE: must use uppercase WINDIR, to work around bugs in
+                # 1.5.2's os.environ.get()
+                windir = os.environ.get("WINDIR")
+                if windir:
+                    tgt_dir = os.path.join(windir, "fonts")
+            elif sys.platform in ("linux", "linux2"):
+                lindirs = os.environ.get("XDG_DATA_DIRS", "")
+                if not lindirs:
+                    # According to the freedesktop spec, XDG_DATA_DIRS should
+                    # default to /usr/share
+                    tgt_dir = "/usr/share/fonts"
+                else: 
+                    lindir = lindirs.split(":")[0]
+                    tgt_dir = os.path.join(lindir, "fonts")
+            elif sys.platform == "darwin":
+                tgt_dir = os.path.expanduser("~/Library/Fonts")
+            
+            if not os.path.isdir(tgt_dir):
+                print('WARNING: Could not locate fonts directory. Default font will be used')
+            else:
+                _src_dir = 'fonts/'
+                _font_file = 'Roboto-Regular.ttf'
+
+                if _font_file not in os.listdir(tgt_dir):
+                    shutil.copyfile(os.path.join(_src_dir, _font_file), os.path.join(tgt_dir, _font_file))
+
+        except:
+            print('WARNING: An issue occured while installing the custom fonts. Default font will be used')    
 
 requirements = [
     'boto3', 'botocore', 'amazon-textract-response-parser>=0.1.9',
@@ -53,4 +93,5 @@ setup(name='amazon-textract-helper',
           'Programming Language :: Python :: 3.8',
           'Programming Language :: Python :: 3.9',
       ],
+      cmdclass={'install': FontInstaller},      
       python_requires='>=3.6')
