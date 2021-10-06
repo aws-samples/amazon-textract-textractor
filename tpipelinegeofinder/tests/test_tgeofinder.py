8c4b7d69390e228c268ff86f7fe04180d3d43d63
schadem <45048633+schadem@users.noreply.github.com>
2021-10-06 14:19:59 -0700

    wip: geofinder added test

--- tpipelinegeofinder/tests/test_tgeofinder.py
+++ tpipelinegeofinder/tests/test_tgeofinder.py
@@ -1,8 +1,7 @@
 import os
 from textractgeofinder.ocrdb import AreaSelection
 from typing import List
-from textractgeofinder.tgeofinder import SelectionElement, NoPhraseForAreaFoundError
-from textractgeofinder.tword import TWord
+from textractgeofinder.tgeofinder import SelectionElement, NoPhraseForAreaFoundError, PhraseCoordinate, PointValueType
 import trp.trp2 as t2
 import textractgeofinder.tgeofinder as tq
 import json
@@ -75,20 +74,12 @@ def test_get_keys_in_area(caplog):
         qdoc = tq.TQuery(json.load(input_fp), doc_height=doc_height, doc_width=doc_width)
         patient_information = qdoc.find_phrase_in_lines("patient information")[0]
         emergency_contact_1 = qdoc.find_phrase_in_lines("emergency contact 1")[0]
-        emergency_contact_2 = qdoc.find_phrase_in_lines("emergency contact 2")[0]
         top_left = t2.TPoint(y=patient_information.ymax, x=0)
         lower_right = t2.TPoint(y=emergency_contact_1.ymin, x=doc_width)
         form_fields = qdoc.get_form_fields_in_area(
             area_selection=AreaSelection(top_left=top_left, lower_right=lower_right))
-        # sel_values: List[SelectionElement] = qdoc.get_selection_values_in_area(area_selection=AreaSelection(
-        #     top_left=top_left, lower_right=lower_right),
-        #                                                                        exclude_ids=[])
-        # for s in form_fields:
-        #     print(s)
-        # to see the log let the test fail
         assert form_fields
         assert len(form_fields) == 11
-        # assert (False)
 
 
 def test_get_selection_values_in_area(caplog):
@@ -101,19 +92,23 @@ def test_get_selection_values_in_area(caplog):
     doc_width = 1000
     with open(os.path.join(SCRIPT_DIR, input_filename)) as input_fp:
         qdoc = tq.TQuery(json.load(input_fp), doc_height=doc_height, doc_width=doc_width)
-        fever_question = qdoc.find_phrase_in_lines("did you feel fever or feverish today")[0]
+        fever_question = qdoc.find_phrase_in_lines("did you feel fever or feverish lately")[0]
         top_left = t2.TPoint(y=fever_question.ymin - 50, x=0)
         lower_right = t2.TPoint(y=fever_question.ymax + 50, x=doc_width)
         sel_values: List[SelectionElement] = qdoc.get_selection_values_in_area(area_selection=AreaSelection(
             top_left=top_left, lower_right=lower_right),
                                                                                exclude_ids=[])
-        print(top_left, lower_right)
-        for s in sel_values:
-            print(s)
-        # to see the log let the test fail
         assert sel_values
         assert len(sel_values) == 2
-        # assert (False)
+
+        cough_question = qdoc.find_phrase_in_lines("do you have a cough")[0]
+        top_left = t2.TPoint(y=cough_question.ymin - 50, x=0)
+        lower_right = t2.TPoint(y=cough_question.ymax + 50, x=doc_width)
+        sel_values: List[SelectionElement] = qdoc.get_selection_values_in_area(area_selection=AreaSelection(
+            top_left=top_left, lower_right=lower_right),
+                                                                               exclude_ids=[])
+        assert sel_values
+        assert len(sel_values) == 2
 
 
 def test_phrase_coordinates(caplog):
@@ -124,39 +119,11 @@ def test_phrase_coordinates(caplog):
     input_filename = os.path.join(SCRIPT_DIR, "data/patient_intake_form_sample.json")
     with open(os.path.join(SCRIPT_DIR, input_filename)) as input_fp:
         qdoc = tq.TQuery(json.load(input_fp), doc_height=1000, doc_width=1000)
-        qdoc.get_values_for_phrase_coordinate([
-            PhraseCoordinate(phrase="Co-Borrower's Name (include Jr. or Sr. if applicable)",
-                             coordinate=PointValueType.XMIN,
-                             min_textdistance=0.95),
-            PhraseCoordinate(phrase="Co Borrower's Name (include Jr. or Sr. if applicable)",
-                             coordinate=PointValueType.XMIN,
-                             min_textdistance=0.92),
+        r: list[float] = qdoc.get_values_for_phrase_coordinate([
+            PhraseCoordinate(phrase="patient information", coordinate=PointValueType.YMIN, min_textdistance=0.95),
+            PhraseCoordinate(phrase="emergency contact 1:", coordinate=PointValueType.YMIN, min_textdistance=0.92),
         ])
-
-    pass
-
-
-def test_get_area(caplog):
-    caplog.set_level(logging.DEBUG)
-    caplog.set_level(logging.DEBUG, logger='textractquery')
-
-    SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
-    input_filename = os.path.join(SCRIPT_DIR, "data/mortgage-1008-selection-samples.json")
-    with open(os.path.join(SCRIPT_DIR, input_filename)) as input_fp:
-        qdoc = tq.TQuery(json.load(input_fp), doc_height=1000, doc_width=1000)
-        occupancy_status_list = qdoc.find_phrase_in_lines("occupancy status")
-        occupancy_status = occupancy_status_list[0]
-        cpm_project_list = qdoc.find_phrase_in_lines("cpm project id")
-        cpm_project = cpm_project_list[0]
-        investment_property = qdoc.find_phrase_on_page(phrase="investment property")[0]
-        additional_property_information_list = qdoc.find_phrase_in_lines("additional property information")
-        additional_property_information = additional_property_information_list[0]
-
-        top_left = t2.TPoint(y=occupancy_status.ymax, x=occupancy_status.xmin)
-        lower_right = t2.TPoint(x=additional_property_information.xmin, y=cpm_project.ymin)
-        twords: List[TWord] = qdoc.get_area(area_selection=AreaSelection(top_left=top_left, lower_right=lower_right),
-                                            exclude_ids=[])
-        assert len(twords) == 9
+        assert r
 
 
 def test_find_word_on_page(caplog):
