8f22194e86e4ff5e9d7e67f10291b045bdea3851
schadem <45048633+schadem@users.noreply.github.com>
2022-02-03 12:56:50 -0800

    update to Pillow 9.0.1

--- tpipelinepagedimensions/setup.py
+++ tpipelinepagedimensions/setup.py
@@ -7,13 +7,11 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
-requirements = ['boto3', 'botocore', 'Pillow==9.0.0', 'PyPDF2==1.26.0']
+requirements = ['boto3', 'botocore', 'Pillow==9.0.1', 'PyPDF2==1.26.0']
 
 if sys.argv[-1] == 'publish-test':
     os.system(f"cd {os.path.dirname(__file__)}")
-    os.system(
-        'rm -rf dist/ build/ amazon_textract_pipeline_pagedimensions.egg-info/'
-    )
+    os.system('rm -rf dist/ build/ amazon_textract_pipeline_pagedimensions.egg-info/')
     os.system('python setup.py sdist bdist_wheel')
     os.system('twine check dist/*')
     os.system('twine upload --repository pypitest dist/*')
@@ -21,40 +19,34 @@ if sys.argv[-1] == 'publish-test':
 
 if sys.argv[-1] == 'publish':
     os.system(f"cd {os.path.dirname(__file__)}")
-    os.system(
-        'rm -rf dist/ build/ amazon_textract_pipeline_pagedimensions.egg-info/'
-    )
+    os.system('rm -rf dist/ build/ amazon_textract_pipeline_pagedimensions.egg-info/')
     os.system('python setup.py sdist bdist_wheel')
     os.system('twine check dist/*')
     os.system('twine upload --repository pypi dist/*')
     sys.exit()
 
-setup(
-    name='amazon-textract-pipeline-pagedimensions',
-    packages=find_packages(exclude=['tests']),
-    include_package_data=True,
-    exclude_package_data={"": ["test_*.py", "__pycache__"]},
-    version='0.0.4',
-    description=
-    'Amazon Textract Pipeline Component to add page dimensions to page block types',
-    install_requires=requirements,
-    scripts=['bin/amazon-textract-pipeline-pagedimensions'],
-    long_description_content_type='text/markdown',
-    long_description=read('README.md'),
-    author='Amazon Rekognition Textract Demoes',
-    author_email='rekognition-textract-demos@amazon.com',
-    url=
-    'https://github.com/aws-samples/amazon-textract-textractor/tree/master/tpipelinepagedimensions',
-    keywords=
-    'amazon-textract-textractor amazon textract textractor pipeline page dimensions',
-    license="Apache License Version 2.0",
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "Topic :: Utilities",
-        'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-    ],
-    python_requires='>=3.6')
+setup(name='amazon-textract-pipeline-pagedimensions',
+      packages=find_packages(exclude=['tests']),
+      include_package_data=True,
+      exclude_package_data={"": ["test_*.py", "__pycache__"]},
+      version='0.0.4',
+      description='Amazon Textract Pipeline Component to add page dimensions to page block types',
+      install_requires=requirements,
+      scripts=['bin/amazon-textract-pipeline-pagedimensions'],
+      long_description_content_type='text/markdown',
+      long_description=read('README.md'),
+      author='Amazon Rekognition Textract Demoes',
+      author_email='rekognition-textract-demos@amazon.com',
+      url='https://github.com/aws-samples/amazon-textract-textractor/tree/master/tpipelinepagedimensions',
+      keywords='amazon-textract-textractor amazon textract textractor pipeline page dimensions',
+      license="Apache License Version 2.0",
+      classifiers=[
+          "Development Status :: 4 - Beta",
+          "Topic :: Utilities",
+          'License :: OSI Approved :: Apache Software License',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8',
+          'Programming Language :: Python :: 3.9',
+      ],
+      python_requires='>=3.6')
