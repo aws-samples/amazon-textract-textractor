b59b55616174ce9249a80e13861307c02934bfa4
Edouard Belval <belvae@amazon.com>
2023-12-11 15:42:31 -0800

    Version 1.4.7

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ def read_requirements(path):
 setup(
     # include data files
     name="amazon-textract-textractor",
-    version="1.4.6",
+    version="1.4.7",
     license="Apache 2.0",
     description="A package to use AWS Textract services.",
     url="https://github.com/aws-samples/amazon-textract-textractor",
