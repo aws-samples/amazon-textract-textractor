3398444a0516dbdd2d78981e66eed3c41bca9e9a
schadem <45048633+schadem@users.noreply.github.com>
2024-02-08 13:12:21 +0100

    bumpversion prettyprinter to 0.1.9

--- prettyprinter/setup.py
+++ prettyprinter/setup.py
@@ -27,7 +27,7 @@ if sys.argv[-1] == 'publish':
 
 setup(name='amazon-textract-prettyprinter',
       packages=['textractprettyprinter'],
-      version='0.1.8',
+      version='0.1.9',
       description='Amazon Textract Helper tools for pretty printing',
       install_requires=requirements,
       long_description_content_type='text/markdown',
